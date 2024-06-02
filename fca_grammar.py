84f9125a3dd814a930866902522db56ba9937243
Diogo Duarte <100220340+diogod1@users.noreply.github.com>
2024-06-02 14:11:46 +0000

    Funcao started

--- fca_grammar.py
+++ fca_grammar.py
@@ -44,6 +44,7 @@ class ArithGrammar:
     def p_declaracao(self, p):
         """declaracao : declaracao_atribuicao
                       | declaracao_escrever
+                      | declaracao_funcao
                       | declaracao_comentario"""
         p[0] = p[1]
  
@@ -64,7 +65,11 @@ class ArithGrammar:
     def p_declaracao_comentario(self,p):
         """declaracao_comentario : COMENTARIOS"""
         p[0] = {'op': 'comentario', 'args': [p[1]]}
-
+        
+    def p_declaracao_funcao(self, p):
+        """declaracao_funcao : FUNCAO ID '(' expressao_funcao ')' ':' expressao ';' """
+        p[0] = {'op': 'funcao', 'args': [p[2],p[4],p[7]]}
+            
     def p_expressao_number(self, p):
         """expressao : NUMBER"""
         p[0] = {'op': 'number', 'args': p[1]}
@@ -92,6 +97,16 @@ class ArithGrammar:
                      | expressao '/' expressao
                      | expressao '*' expressao"""
         p[0] = {'op': p[2], 'args': [p[1], p[3]]}
+    
+    def p_expressao_funcao(self, p):
+        """expressao_funcao : expressao
+                            | expressao_funcao ',' expressao"""
+        if len(p) == 2:
+            p[0] = {'op': 'expressao_funcao', 'args': [p[1]]}
+        else:
+            p[1]['args'].append(p[3])
+            p[0] = p[1]
+    
     
     # def p_declaracao_funcao(self, p):
     #     """declaracao_funcao: FUNCAO ID'('lista_atribuicao')' = lista_expressoes ';'"""
